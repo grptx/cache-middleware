7f27e6ddaa7e6b528e412944d2fb9115651af5f5
Roberto Rossetti <roberto.rossetti@unicaspa.it>
2021-02-20 16:11:22 +0100

    async support

--- lib/HttpCacheControl.ts
+++ lib/HttpCacheControl.ts
@@ -13,9 +13,9 @@ export class HttpCacheControl {
         const lastModified: Date = await f();
         res.setHeader("Last-Modified", lastModified.toUTCString());
         if (_req.headers["if-modified-since"]) {
-          if (_req.headers["if-modified-since"] < lastModified.toUTCString()) {
+          if (_req.headers["if-modified-since"] >= lastModified.toUTCString()) {
             res.status(304).send();
-            next("route");
+            return next("route");
           }
         }
       }
@@ -23,15 +23,19 @@ export class HttpCacheControl {
     };
   }
 
-  public static Etag(f: () => string) {
-    return (_req: Request, res: Response, next: NextFunction): void => {
+  public static Etag(f: () => Promise<string> | string) {
+    return async (
+      _req: Request,
+      res: Response,
+      next: NextFunction
+    ): Promise<void> => {
       if (f) {
-        const _etag: string = f();
+        const _etag: string = await f();
         res.setHeader("Etag", etag(String(_etag)));
         if (_req.headers["if-none-match"]) {
           if (_req.headers["if-none-match"] === String(etag)) {
             res.status(304).send();
-            next("route");
+            return next("route");
           }
         }
       }
@@ -39,10 +43,14 @@ export class HttpCacheControl {
     };
   }
 
-  public static MaxAge(f: () => number) {
-    return (_req: Request, res: Response, next: NextFunction): void => {
+  public static MaxAge(f: () => Promise<number> | number) {
+    return async (
+      _req: Request,
+      res: Response,
+      next: NextFunction
+    ): Promise<void> => {
       if (f) {
-        const age: number = f();
+        const age: number = await f();
         res.setHeader("Cache-Control", `max-age=${String(age)}`);
       }
       next();
