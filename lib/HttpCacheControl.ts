64d9c1389f048291e24347641559c955fa56ff3d
Roberto Rossetti <roberto.rossetti@unicaspa.it>
2021-02-20 09:00:39 +0100

    etag header

--- lib/HttpCacheControl.ts
+++ lib/HttpCacheControl.ts
@@ -2,13 +2,29 @@ import { Request, Response } from 'express-serve-static-core';
 import { NextFunction } from 'express';
 
 export class HttpCacheControl {
-    public static LastModified(f:Function) {
+    public static LastModified(f: Function) {
         return (_req: Request, res: Response, next: NextFunction): void => {
             if (f) {
                 const lastModified: Date = f();
-                res.setHeader('Last-Modified',lastModified.toUTCString());
+                res.setHeader('Last-Modified', lastModified.toUTCString());
                 if (_req.headers['if-modified-since']) {
-                    if(_req.headers['if-modified-since']<lastModified.toUTCString()){
+                    if (_req.headers['if-modified-since'] < lastModified.toUTCString()) {
+                        res.status(304).send();
+                        next('route');
+                    }
+                }
+            }
+            next();
+        };
+    }
+
+    public static Etag(f: Function) {
+        return (_req: Request, res: Response, next: NextFunction): void => {
+            if (f) {
+                const etag: string = f();
+                res.setHeader('Etag', String(etag));
+                if (_req.headers["if-none-match"]) {
+                    if (_req.headers['if-none-match'] === String(etag)) {
                         res.status(304).send();
                         next('route');
                     }
