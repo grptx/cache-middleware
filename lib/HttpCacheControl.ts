62e9fc359c82918dc2dd42e5abee218a89b9511e
Roberto Rossetti <roberto.rossetti@unicaspa.it>
2021-02-20 14:57:38 +0100

    bug fix

--- lib/HttpCacheControl.ts
+++ lib/HttpCacheControl.ts
@@ -39,7 +39,7 @@ export class HttpCacheControl {
     return (_req: Request, res: Response, next: NextFunction): void => {
       if (f) {
         const age: number = f();
-        res.setHeader("Cache-Control", String(age));
+        res.setHeader("Cache-Control", `max-age=${String(age)}`);
       }
       next();
     };
